8d139f73054f9fe3e7b825b69c01e78e23d4ba48
Richard Fisher <richardfsr@gmail.com>
2022-04-02 13:10:23 +0800

    reverting to finalizing exchange by using get auctions instead of get sales because get sales hasn't been reliable recently

--- app/jobs/finalize_exchange_job.rb
+++ app/jobs/finalize_exchange_job.rb
@@ -12,32 +12,49 @@ class FinalizeExchangeJob < ApplicationJob
         row.finalized = true
         row.save
       else
-        response = fetch_from_exchange(row['mint'])
-        update_sale(response[0], row) unless response.nil?
+        response = fetch_from_exchange(row['collection_name'], 0)
+        next if response.nil?
+        next if find_mint(response['auctions'], row)
+
+        remaining = response['count'] - 20
+        from = 20
+        while remaining.positive?
+          response = fetch_from_exchange(row['collection_name'], from)
+          remaining -= 20
+          from += 20
+          remaining = 0 if find_mint(response['auctions'], row)
+        end
       end
     end
     FinalizeExchangeJob.delay(run_at: 5.minutes.from_now).perform_later
   end
 
-  def update_sale(sale, row)
-    return if sale.nil?
+  def find_mint(auctions, row)
+    return if auctions.nil?
 
-    row.highest_bid = sale['amount']
-    row.highest_bidder = sale['to']
-    row.finalized = true
-    row.save
+    found = false
+    auctions.each do |auction|
+      next unless auction['keys']['mint'] == row.mint
+
+      row.end_time = auction['data']['end']
+      row.highest_bid = auction['data']['highestBid']
+      row.highest_bidder = auction['data']['highestBidder']
+      row.number_bids = auction['data']['numberBids']
+      row.finalized = true
+      row.save
+      found = true
+    end
+    found
   end
 
-  def finalize_now(row)
-    row.update_attribute :finalized, true
-  end
-
-  def fetch_from_exchange(mint)
-    result = HTTParty.post(ENV['EXCHANGE_SALES'],
+  def fetch_from_exchange(collection, from)
+    result = HTTParty.post(ENV['EXCHANGE_AUCTIONS'],
                            body: {
+                             from: from,
+                             size: 20,
                              query: {
                                filters: {
-                                 mint: mint
+                                 collections: [collection]
                                }
                              }
                            }.to_json,
